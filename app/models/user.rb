4ea8818bc3932609288620cebe465054a9ea89ce
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-10 02:12:58 +0900

    ユーザー新規登録のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :last_name
+    validates :first_name
+    validates :kana_last_name
+    validates :kana_first_name
+    validates :password
+    validates :birthday
+  end
+
+
+  with_options allow_blank: true do
+    validates :last_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :kana_last_name, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :kana_first_name, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :password, format: {with: /\A(?=.*?[a-zA-Z])(?=.*?[\d])[a-z\d]+\z/}
+  end
+  
 end
