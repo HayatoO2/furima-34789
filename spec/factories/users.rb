38ca7addca64ea834af1f2db29d4efe9153ffb5b
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-10 12:34:29 +0900

    テストの準備ー修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
-  password = Faker::Internet.password(min_length: 6)
-
+  
   factory :user do
     nickname { Faker::Name.name }
     last_name { 'カー寝る' }
@@ -8,8 +7,8 @@ FactoryBot.define do
     kana_last_name { Gimei.last.katakana }
     kana_first_name { Gimei.first.katakana }
     email { Faker::Internet.free_email }
-    password { password }
-    password_confirmation { password }
+    password {"111aaa"}
+    password_confirmation {password}
     birthday { Faker::Date.between(from: '1930-01-01', to: Date.today) }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
+  describe 'ユーザー登録機能' do
+    it 'メールアドレスは必須' do
+    end
+  end
 end
