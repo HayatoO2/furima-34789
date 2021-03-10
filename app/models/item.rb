a91b66e2a40533c0b54ebd526db1b123145670a9
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-11 06:48:48 +0900

    priceのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,15 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  with_options numericalit: {other_than: 1} do
+  with_options numericalit: {other_than: 1}, allow_blank: true do
     validates :category_id
     validates :status_id
     validates :pay_id
     validates :prefecture_id
     validates :delivery_date_id
   end
+
+  validates :price, allow_blank: true, numericality:  { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
 
 
 
