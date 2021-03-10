fc61325404680fa432c5b5c6106d21402376e495
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-10 13:26:21 +0900

    テストー'パスワードとパスワード（確認用）、値が不一致だと登録できない'

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,32 +11,43 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー登録機能' do
+
     it '適切な値を入力したら登録できる' do
       @user.valid?
       expect(@user).to be_valid
     end
+
     it 'メールアドレスは必須' do
       @user.email = ''
       error_check("Email can't be blank")
     end
+
     it 'メールアドレスに一意性あり' do
       user = FactoryBot.create(:user)
       @user.email = user.email
       @user.valid?
       error_check("Email has already been taken")
     end
+
     it 'メールアドレスに＠がないと登録できない' do
       @user.email = @user.email.gsub(/@/,"a")
       error_check("Email is invalid")
     end
+
     it 'パスワードが空だと登録できない' do
       @user.password = ''
       error_check("Password can't be blank")
     end
+
     it 'パスワードは６文字以上じゃないと登録できない' do
       @user.password = '111aa'
       @user.password_confirmation = @user.password
       error_check("Password is too short (minimum is 6 characters)")
+    end
+
+    it 'パスワードとパスワード（確認用）、値が不一致だと登録できない' do
+      @user.password_confirmation = @user.password + "aaa"
+      error_check("Password confirmation doesn't match Password")
     end
   end
 end
