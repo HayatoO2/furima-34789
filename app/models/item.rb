9a7923ca943823bc541ca08641f95c1323715ed1
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-11 08:31:53 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
 
 
-  belongs_to :user
+  
 
   with_options presence: true do 
+    validates :image
     validates :name
     validates :text
     validates :category_id
@@ -14,7 +15,7 @@ class Item < ApplicationRecord
     validates :price
   end
 
-  with_options numericality: {other_than: 1}, allow_blank: true do
+  with_options numericality: {other_than: 1}  do
     validates :category_id
     validates :status_id
     validates :pay_id
@@ -22,10 +23,10 @@ class Item < ApplicationRecord
     validates :delivery_date_id
   end
 
-  validates :price, allow_blank: true, numericality:  { greater_than_or_equal_to: 300 }, numericality: { less_than_or_equal_to: 9999999 }
+  validates :price, allow_blank: true, numericality:  { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   has_one_attached :image
-
+  belongs_to :user
 
 
 
