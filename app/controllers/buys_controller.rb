4c3e7e313b3113e4eb6067af1fef1062fa470969
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-12 16:21:28 +0900

    購入画面の表示の実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,7 +1,10 @@
 class BuysController < ApplicationController
 
   def index
+    
     @buy_address = BuyAddress.new
+    @item = Item.find(params[:item_id])
   end
+
 
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -1,9 +1,9 @@
 class BuyAddress
   include ActiveModel::Model
 
-  attr_accessor :user_id, :item_id, :telephone :post_num, :prefecture_id, :city, :house_num, :building, :buy_id
+  attr_accessor :user_id, :item_id, :telephone, :post_num, :prefecture_id, :city, :house_num, :building, :buy_id
 
-  with_options presence: trure do
+  with_options presence: true do
     validates :telephone
     validates :post_num
     validates :prefecture_id
@@ -16,6 +16,7 @@ class BuyAddress
 
   def save
     buy = Buy.create(user_id: user_id, item: item_id)
+
     Address.create(telephone: telephone, post_num: post_num, prefecture_id: prefecture_id, city: city, house_num: house_num, building: building, buy_id: buy.id)
   end
 end
