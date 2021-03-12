20cb75e930978e1eb4866ddf787f7f4a89f799e4
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-12 14:42:18 +0900

    addressモデルにバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+
+  with_options presence: trure do
+    validates :telephone
+    validates :post_num
+    validates :prefecture_id
+    validates :city
+    validates :house_num
+    validates :buy
+  end
 end
