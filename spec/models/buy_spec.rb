28bc9690fd155ad7769f5861ba6fbbab901a4127
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-14 09:20:02 +0900

    テスト項目の列挙

--- spec/models/buy_spec.rb
+++ spec/models/buy_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Buy, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品購入機能' do
+    context '商品購入できる' do
+      it '適切な値を入力したら商品購入できる' do
+      end
+
+      it 'buildingが未入力でも商品購入できる' do
+      end
+    end
+
+    context '商品購入できない' do
+      it 'tokenが空だと購入できない' do
+      end
+      it 'post_numが未入力だと購入できない' do
+      end
+      it 'post_numが-なしだと購入できない' do
+      end
+      it 'prefecture_idが1だと購入できない' do
+      end
+      it 'cityが未入力だと購入できない' do
+      end
+      it 'house_numが未入力だと購入できない' do
+      end
+      it 'telephoneが未入力だと購入できない' do
+      it 'telephoneが全角数字だと購入できない' do
+      end
+      it 'telephoneが12桁以上だと購入できない' do
+      end
+    end
+  end
 end
