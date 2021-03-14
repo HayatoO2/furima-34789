e91bf8bc4c7bd017cd25dfc71e354ae471ec2290
hayato <shu_mon_win2@yahoo.co.jp>
2021-03-14 15:54:06 +0900

    edit画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   
 
-  resources :items, only: [:index, :new, :create, :show] do
+  resources :items, only: [:index, :new, :create, :show, :edit] do
     resources :buys, only: [:index, :create]
   end
   root to: "items#index"
